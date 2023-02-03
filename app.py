b4a1e946786aa8ea7d30570dd5715880a442731e
Dilusha Dasanayaka <dilusha.dasanayaka_ne@ntucguest.com>
2023-02-03 20:55:30 +0530

    get image from request & cors

--- app.py
+++ app.py
@@ -20,21 +20,27 @@
 
 # app.py
 
-from flask import Flask
+from flask import Flask, request
 from PIL import Image
 import pytesseract
 import json
 import requests
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def hello_world():
     return "Hello, World!"
 
-@app.route('/ocr')
+@app.route('/ocr', methods=["POST"])
 def index():
- url='https://docs.unity3d.com/Packages/com.unity.textmeshpro@3.2/manual/images/TMP_RichTextLineIndent.png'
- image = Image.open(requests.get(url, stream=True).raw)
+ if request.files.get("image") is None:
+    return {'error': 'Image is missing'}
+#  url='https://docs.unity3d.com/Packages/com.unity.textmeshpro@3.2/manual/images/TMP_RichTextLineIndent.png'
+#  image = Image.open(requests.get(url, stream=True).raw)
+ image = Image.open(request.files["image"])
  context = {'content' : pytesseract.image_to_string(image)}
  return context
